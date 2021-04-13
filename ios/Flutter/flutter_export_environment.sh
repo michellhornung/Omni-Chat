200fe80d7fa1a9400b122a2533b09f50dc1de10d
Michell Hornung <michellhornung@gmail.com>
2021-04-13 16:25:36 -0300

    changed folder
    changed folder

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/michellhornung/DEV/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/michellhornung/DEV/workspaces/flutter/flash-chat-flutter"
-export "FLUTTER_TARGET=/Users/michellhornung/DEV/workspaces/flutter/flash-chat-flutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/michellhornung/DEV/workspaces/py/flash_chat"
+export "FLUTTER_TARGET=/Users/michellhornung/DEV/workspaces/py/flash_chat/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/michellhornung/DEV/workspaces/flutter/flash-chat-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/michellhornung/DEV/workspaces/py/flash_chat/.dart_tool/package_config.json"
