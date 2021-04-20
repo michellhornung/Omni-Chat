276e8f10081d4e8413f3df7189f99c3279139785
Michell Hornung <michellhornung@gmail.com>
2021-04-20 11:58:17 -0300

    Tabs and list
    Tabs and list

--- lib/screens/chats_recebidos_screen.dart
+++ lib/screens/chats_recebidos_screen.dart
@@ -15,33 +15,119 @@ class _ChatsRecebidosScreenState extends State<ChatsRecebidosScreen> {
     return MaterialApp(
       home: DefaultTabController(
         length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.card_travel)),
-                Tab(icon: Icon(Icons.add_shopping_cart)),
-              ],
-            ),
-            title: Text('Tabs Demo'),
+        child: new Scaffold(
+          appBar: new AppBar(
+            title: const Text('Chats Recebidos'),
+            bottom: new TabBar(isScrollable: true, tabs: [
+              new Tab(text: 'Em Atendimento', icon: new Icon(Icons.person)),
+              new Tab(text: 'Internos', icon: new Icon(Icons.contact_phone)),
+            ]),
           ),
-          body: TabBarView(
+          body: new TabBarView(
             children: [
-              Center(
-                  child: Text(
-                    "0",
-                    style: TextStyle(fontSize: 40),
-                  )),
-              Center(
-                  child: Text(
-                    "1",
-                    style: TextStyle(fontSize: 40),
-                  )),
+              new ListView(
+                children: list,
+              ),
+              new ListView(
+                children: list,
+              ),
             ],
-            ),
           ),
         ),
+      ),
     );
-
   }
 }
+List<Widget> list = <Widget>[
+  new ListTile(
+    title: new Text('CineArts at the Empire',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('85 W Portal Ave'),
+    leading: new Icon(
+      Icons.theaters,
+      color: Colors.blue[500],
+    ),
+  ),
+  new ListTile(
+    title: new Text('The Castro Theater',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('429 Castro St'),
+    leading: new Icon(
+      Icons.theaters,
+      color: Colors.blue[500],
+    ),
+  ),
+  new ListTile(
+    title: new Text('Alamo Drafthouse Cinema',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('2550 Mission St'),
+    leading: new Icon(
+      Icons.theaters,
+      color: Colors.blue[500],
+    ),
+  ),
+  new ListTile(
+    title: new Text('Roxie Theater',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('3117 16th St'),
+    leading: new Icon(
+      Icons.theaters,
+      color: Colors.blue[500],
+    ),
+  ),
+  new ListTile(
+    title: new Text('United Artists Stonestown Twin',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('501 Buckingham Way'),
+    leading: new Icon(
+      Icons.theaters,
+      color: Colors.blue[500],
+    ),
+  ),
+  new ListTile(
+    title: new Text('AMC Metreon 16',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('135 4th St #3000'),
+    leading: new Icon(
+      Icons.theaters,
+      color: Colors.blue[500],
+    ),
+  ),
+  new Divider(),
+  new ListTile(
+    title: new Text('K\'s Kitchen',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('757 Monterey Blvd'),
+    leading: new Icon(
+      Icons.restaurant,
+      color: Colors.blue[500],
+    ),
+  ),
+  new ListTile(
+    title: new Text('Emmy\'s Restaurant',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('1923 Ocean Ave'),
+    leading: new Icon(
+      Icons.restaurant,
+      color: Colors.blue[500],
+    ),
+  ),
+  new ListTile(
+    title: new Text('Chaiya Thai Restaurant',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('272 Claremont Blvd'),
+    leading: new Icon(
+      Icons.restaurant,
+      color: Colors.blue[500],
+    ),
+  ),
+  new ListTile(
+    title: new Text('La Ciccia',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('291 30th St'),
+    leading: new Icon(
+      Icons.restaurant,
+      color: Colors.blue[500],
+    ),
+  ),
+];
