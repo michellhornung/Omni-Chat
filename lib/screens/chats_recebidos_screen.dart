c43773d5520d363685e4c05e9d97a8befc5331de
Michell Hornung <michellhornung@gmail.com>
2021-04-22 11:54:40 -0300

    appbar changes
    appbar changes

--- lib/screens/chats_recebidos_screen.dart
+++ lib/screens/chats_recebidos_screen.dart
@@ -17,11 +17,22 @@ class _ChatsRecebidosScreenState extends State<ChatsRecebidosScreen> {
         length: 2,
         child: new Scaffold(
           appBar: new AppBar(
-            title: const Text('Chats Recebidos'),
-            bottom: new TabBar(isScrollable: true, tabs: [
-              new Tab(text: 'Em Atendimento', icon: new Icon(Icons.person)),
-              new Tab(text: 'Internos', icon: new Icon(Icons.contact_phone)),
-            ]),
+            backgroundColor: Colors.white,
+            title:
+              Container(
+                //width: 40,
+                height: 200,
+                child: Image.asset('images/logo-lhtec.png'),
+              ),
+              centerTitle: true,
+            //leading: Icon(Icons.account_circle_rounded),
+            leadingWidth: 100,
+            toolbarHeight: 100,
+            //title: const Text('Chats Recebidos'),
+            // //bottom: new TabBar(isScrollable: true, tabs: [
+            //   new Tab(text: 'Em Atendimento', icon: new Icon(Icons.person)),
+            //   new Tab(text: 'Internos', icon: new Icon(Icons.contact_phone)),
+            // ]),
           ),
           body: new TabBarView(
             children: [
